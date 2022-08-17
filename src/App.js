5d51fdc16557d40d17f1bbd603155d5e8477ce45
Rohan Magar <rohan.magar.415@gmail.com>
2022-08-17 08:06:16 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import Home from './routes/Home';
+import Home from "./components/HeroImg";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Mens from './routes/Mens';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mens from "./components/Heroimg2";
 function App() {
   return (
     <>
-    <Router>
-    <Routes>
-        <Route path="/home" element={<Home  />} />
-        <Route path="mens" element={<Mens />} />
-      </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mens" element={<Mens />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,29 +1,38 @@
-import './Herostyle.css';
-import React from 'react';
+import "./Herostyle.css";
+import React from "react";
 import ProductImg from "../assets/user.png";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const HeroImg = () => {
   return (
     <>
-       <div className='hero-item'>
-         <div className="main-content">
+      <div className="hero-item" style={{ position: "relative" }}>
+        <Navbar />
+        <div className="main-content">
           <div className="heading">
-              <h1>Great Discount on Latest Collection</h1>
+            <h1>Great Discount on Latest Collection</h1>
           </div>
           <div className="para">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse explicabo expedita debitis dicta sequi accusamus magnam sed vel aut reprehenderit repudiandae fugiat suscipit cupiditate, officia quis eaque, perferendis rerum! Nam?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              explicabo expedita debitis dicta sequi accusamus magnam sed vel
+              aut reprehenderit repudiandae fugiat suscipit cupiditate, officia
+              quis eaque, perferendis rerum! Nam?
+            </p>
           </div>
           <div className="button">
-              <Link to="#">Shop now</Link>
+            <Link to="#">Shop now</Link>
           </div>
+        </div>
+        <div className="images">
+          <img className="girl-img" src={ProductImg} alt="ProductImg" />
+        </div>
       </div>
-      <div className="images">
-      <img className="girl-img" src={ProductImg} alt="ProductImg" />
-      </div>
-      </div>
-      </>
-  )
-}
+      <Footer />
+    </>
+  );
+};
 
-export default HeroImg
+export default HeroImg;

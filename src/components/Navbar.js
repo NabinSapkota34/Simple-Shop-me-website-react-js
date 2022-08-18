d5107f0cbbad5cecdfea10d487d4d8d09cc5b932
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2022-08-18 19:37:32 +0545

    Fixed the link style issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,48 @@
 import "./NavbarStylesheet.css";
-import React ,{useState} from 'react';
+import React, { useState } from "react";
 import IntroImg from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import {FaBars, FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
 
-    
   return (
     <div className="navbar-item">
-        <div className="logo">
-         <img className="into-img" src={IntroImg} alt="IntroImg" />
-        </div>
-        <ul className={click ? "nav-menu active":"nav-menu"}>
-            <li>
-                <Link to="/home" style={{ textDecoration: 'none' }}>Home</Link>
-            </li>
-            <li>
-                <Link to="/mens" style={{ textDecoration: 'none' }}>Men</Link>
-            </li>
-            <li>
-                <Link to="#" style={{ textDecoration: 'none' }}>Women</Link>
-            </li>
-            <li>
-                <Link to="#" style={{ textDecoration: 'none' }}>Kids</Link>
-            </li>
-            <li>
-                <Link to="#" style={{ textDecoration: 'none' }}>Contact Us</Link>
-            </li>
-        </ul>
-        <div className="hamburger" onClick={handleClick}>
-            {click ? (<FaTimes size={20} style={{color: "#3f51b5"}}/>): (<FaBars size={20} style={{color: "#3f51b5"}}/>)}
-            
-            
-        </div>
+      <div className="logo">
+        <img className="into-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      {/* FIXME: this way to set the active style will fail since this will
+      apply active class in all the link at once, try creating the loops of 
+      menu & set active class there.
+      */}
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/mens">Men</Link>
+        </li>
+        <li>
+          <Link to="#">Women</Link>
+        </li>
+        <li>
+          <Link to="#">Kids</Link>
+        </li>
+        <li>
+          <Link to="#">Contact Us</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#3f51b5" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#3f51b5" }} />
+        )}
+      </div>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Navbar
+export default Navbar;
